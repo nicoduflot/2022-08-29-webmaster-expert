a70702974d26a4c1ed3ca4e15f88019064d21bbc
unknown <nduflot@dawan.fr>
2022-08-31 16:57:57 +0200

    bootstrap pt 3 bs et JS modal search

--- js/bs-js.js
+++ js/bs-js.js
@@ -16,6 +16,55 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    function resultSearch(data, search){
+        let tabResult = [];
+        let searchOk = false;
+        let html = '';
+        let currentLine ='';
+        data.forEach(function(enregistrement){
+            currentLine += '<tr>';
+            for(key in enregistrement){
+                currentLine += '<td>';
+                if('object' !== typeof(enregistrement[key])){
+                    
+                    if(enregistrement[key].toString().toLowerCase().indexOf(search.toLowerCase()) >= 0){
+                        currentLine += `<mark>${enregistrement[key]}</mark>`;
+                        searchOk = true
+                    }else{
+                        currentLine += `${enregistrement[key]}`;
+                    }
+                }else{
+                    for(subKey in enregistrement[key]){
+                        if('object' !== typeof(enregistrement[key][subKey])){
+                            if(enregistrement[key][subKey].toString().toLowerCase().indexOf(search.toLowerCase()) >= 0){
+                                currentLine += `<mark>${enregistrement[key][subKey]}</mark><br />`;
+                                searchOk = true
+                            }else{
+                                currentLine += `${enregistrement[key][subKey]}<br />`;
+                            }
+                        }
+                        
+                    }
+                }
+                currentLine += '</td>';
+            }
+            currentLine += '</tr>';
+            if(searchOk){
+                tabResult.push(enregistrement);
+                html += currentLine;
+            }
+            searchOk = false;
+            currentLine = '';
+        });
+        console.log(tabResult);
+        if(tabResult.length === 0){
+            return false;
+        }else{
+            //console.log(html);
+            return html;
+        }
+    }
+
     btnProgress01.addEventListener('click', function () {
         progress01.style.setProperty('width', '0');
         let [valMin, valMax] = [0, 100];//destructuration
@@ -38,6 +87,24 @@ window.addEventListener('DOMContentLoaded', function() {
                 search = elt.value;
                 if(search.length >= 3){
                     console.log(search);
+                    fetch('./ressources/users.json')
+                    .then( function(reponse){
+                        return reponse.json();
+                    })
+                    .then(function(data){
+                        //console.log(data);
+                        let modalResult = document.querySelector('#modalResult');
+                        let result = resultSearch(data, search);
+                        if(result !== ''){
+                            document.querySelector('#resSearchBody').innerHTML = result;
+                            modalResult.click();
+                        }
+                    }).catch(function(error){
+                        console.log(error.message);
+                    })
+                    .finally(function(){
+                        console.log('traitement terminé');
+                    });
                 }
             }
         });
@@ -46,4 +113,17 @@ window.addEventListener('DOMContentLoaded', function() {
     ( function(test){
         console.log(test);
     })('mon test'); // fonction auto exécutée
+
+    /* objet litéral */
+
+    let obj = {
+        nom: 'Duflot',
+        prenom: 'Nicolas',
+        bDP: function(){
+            return 'une blague de papa';
+        }
+    };
+
+    console.log(obj.nom, obj.prenom, obj.bDP());
+
 });
